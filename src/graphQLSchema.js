d63d15b55e56fa80be57590e227d11ed34999074
Cesar Solano <csolanoc@unal.edu.co>
2022-05-01 17:29:46 -0500

    Docker-compose, and import template

--- src/graphQLSchema.js
+++ src/graphQLSchema.js
@@ -3,41 +3,25 @@ import GraphQLJSON from 'graphql-type-json'
 import { makeExecutableSchema } from 'graphql-tools'
 import { mergeSchemas } from './utilities'
 
-//como debemos importar los typeDef
-// import {} from './swarch2022i/imagine_auth_ms/typeDefs';
-// import {} from './swarch2022i/imagine_collection_ms/typeDefs';
-import {
-  imageMutations,
-  imageQueries,
-  imageTypeDef,
-} from './Imagine/imagine_image_ms/typeDefs';
-// import {} from './swarch2022i/imagine_network_ms/typeDefs';
-// import {} from './swarch2022i/imagine_profile_ms/typeDefs';
 
-import {
-  categoryMutations,
-  categoryQueries,
-  categoryTypeDef,
-} from './Imagine/example/typeDefs'
-
-//como debemos importar los resolvers
-// import authResolvers from './swarch2022i/imagine_auth_ms/resolvers';
-// import collectionResolvers from './swarch2022i/imagine_collection_ms/resolvers';
-import imageResolvers from './Imagine/imagine_image_ms/resolvers';
-// import networkResolvers from './swarch2022i/imagine_network_ms/resolvers';
-// import profileResolvers from './swarch2022i/imagine_profile_ms/resolvers';
-
-// import categoryResolvers from './Imagine/example/resolvers'
-
-import imageStorageResolvers from './Imagine/imagine_storage_ms/resolvers'
+//Mutations, Queries and TypeDefs imports
+import { imageMutations, imageQueries, imageTypeDef, } from './Imagine/imagine_image_ms/typeDefs';
 import { imageStorageMutations, imageStorageQueries, imageStorageTypeDef } from './Imagine/imagine_storage_ms/typeDefs'
 
-// merge the typeDefs -- esto nos toca con cada typeDefs
+
+//Resolvers imports
+import imageResolvers from './Imagine/imagine_image_ms/resolvers';
+import imageStorageResolvers from './Imagine/imagine_storage_ms/resolvers'
+
+
+
+// Merge mutations, queries and typdefs
 const mergedTypeDefs = mergeSchemas(
   ['scalar JSON', imageTypeDef, imageStorageTypeDef], [imageQueries, imageStorageQueries], [imageMutations, imageStorageMutations],
 )
 
-// Generate the schema object from your types definition. -- lomismo la enfermedad del lomo :v
+
+// Generate Schema with all above and resolvers
 export default makeExecutableSchema({
   typeDefs: mergedTypeDefs,
   resolvers: merge({ JSON: GraphQLJSON }, // allows scalar JSON
