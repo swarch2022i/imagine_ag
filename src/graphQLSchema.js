59e6cc372ef4b971d0ae55c1b55ac18b51747f55
josteda99 <josteda99@gmail.com>
2022-05-01 17:28:25 -0500

    delete comments

--- src/graphQLSchema.js
+++ src/graphQLSchema.js
@@ -3,10 +3,6 @@ import GraphQLJSON from 'graphql-type-json'
 import { makeExecutableSchema } from 'graphql-tools'
 import { mergeSchemas } from './utilities'
 
-//como debemos importar los typeDef
-// import {} from './Imagine/imagine_auth_ms/typeDefs';
-// import {} from './Imagine/imagine_collection_ms/typeDefs';
-// import {} from './Imagine/imagine_image_ms/typeDefs';
 import {
   commentQueries,
   commentTypeDef,
@@ -18,29 +14,14 @@ import {
   voteMutations,
 } from './Imagine/imagine_network_ms/votes/typeDefs'
 
-// import {} from './Imagine/imagine_profile_ms/typeDefs';
-
-import {
-  categoryMutations,
-  categoryQueries,
-  categoryTypeDef,
-} from './Imagine/example/typeDefs'
-
-//como debemos importar los resolvers
-// import authResolvers from './Imagine/imagine_auth_ms/resolvers';
-// import collectionResolvers from './Imagine/imagine_collection_ms/resolvers';
-// import imageResolvers from './Imagine/imagine_image_ms/resolvers';
 import networkResolvers from './Imagine/imagine_network_ms/comments/resolvers'
 import voteResolvers from './Imagine/imagine_network_ms/votes/resolvers'
-// import profileResolvers from './Imagine/imagine_profile_ms/resolvers';
-
-import categoryResolvers from './Imagine/example/resolvers'
 
 // merge the typeDefs -- esto nos toca con cada typeDefs
 const mergedTypeDefs = mergeSchemas(
-  ['scalar JSON', categoryTypeDef, commentTypeDef, voteTypeDef],
-  [commentQueries, voteQueries, categoryQueries],
-  [commentMutations, voteMutations, categoryMutations],
+  ['scalar JSON', commentTypeDef, voteTypeDef],
+  [commentQueries, voteQueries],
+  [commentMutations, voteMutations],
 )
 
 // Generate the schema object from your types definition. -- lomismo la enfermedad del lomo :v
@@ -48,7 +29,6 @@ export default makeExecutableSchema({
   typeDefs: mergedTypeDefs,
   resolvers: merge([
     { JSON: GraphQLJSON }, // allows scalar JSON
-    categoryResolvers,
     networkResolvers,
     voteResolvers,
   ]),
