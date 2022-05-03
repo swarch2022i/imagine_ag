bc9c4a1493b3deccf06220ab138103e4336e75aa
dacperezce96 <dc.perezc1@uniandes.edu.co>
2022-05-02 23:10:33 -0500

    pruebas

--- src/Imagine/imagine_auth_ms/resolvers.js
+++ src/Imagine/imagine_auth_ms/resolvers.js
@@ -4,20 +4,8 @@ import { url, port, entryPoint } from './server'
 const URL = `http://${url}:${port}/${entryPoint}`
 
 const resolvers = {
-  Query: {
-    allUsers: (_, { token }) => generalRequest(URL, '', token=token),
-    userById: (_, { id, token }) => generalRequest(`${URL}/users/${id}`, 'GET', token=token),
-  },
-  Mutation: {
-    createUser: (_, { user }) =>
-      generalRequest(`${URL}/users`, 'POST', user),
-    updateUser: (_, { id, token, user }) =>
-      generalRequest(`${URL}/users/${id}`, 'PUT', user, token=token),
-    deleteUser: (_, { id, token }) => 
-      generalRequest(`${URL}/users/${id}`, 'DELETE', token=token),
-    login: (_, { login }) =>
-      generalRequest(`${URL}/auth/login`, 'POST', login),
-  },
+  Query: {},
+  Mutation: {},
 }
 
 export default resolvers
--- src/Imagine/imagine_auth_ms/typeDefs.js
+++ src/Imagine/imagine_auth_ms/typeDefs.js
@@ -1,40 +1,5 @@
-export const authTypeDef = `
-type User {
-    id: Int!
-    username: String!
-    password_digest: String!
-    created_at: String!
-    updated_at: String!
-}
-input UserInput {
-    username: String!
-    password: String!
-    password_confirmation: String!
-}
-input UserInputUpdate {
-    username: String!
-    password: String!
-    password_confirmation: String!
-    actual_password: String!
-}
-type Login {
-    username: String!
-    token: String!
-    exp: String!
-}
-input LoginInput {
-    username: String!
-    password: String!
-}`;
+export const authTypeDef = ``;
 
-export const authQueries =`
-    allUsers(token: String!): [User]!
-    userById(id: Int!, token: String!): User!
-`;
+export const authQueries =``;
 
-export const authMutations = `
-    createUser(user: UserInput!): User!
-    updateUser(id: Int!, token: String!, user: UserInputUpdate!): User!
-    deleteUser(id: Int!, token: String!): Void
-    login(login: LoginInput!): Login!
-`;
+export const authMutations = ``;
